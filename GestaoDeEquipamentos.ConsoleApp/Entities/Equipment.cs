99ae0837689cd735d4ef85d4f692d2a75c0ab9e2
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 15:21:40 -0300

    feat: Adicionado atributos na `Equipment.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Equipment.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Equipment.cs
@@ -2,4 +2,10 @@
 
 public class Equipment
 {
+    public string Name;
+    public double AcquisitionPrice;
+    public string SerialNumber;
+    public DateTime FabricDate;
+    public string Manufacturer;
+    public static int id = 0;
 }
