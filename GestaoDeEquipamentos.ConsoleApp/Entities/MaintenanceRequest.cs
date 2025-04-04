181f2e6734ef91e74b127d4bc3c913daae1184f9
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 18:27:05 -0300

    feat: Criada `MaintenanceRequest.cs` para conter os atributos e método dos chamados.

---
+++ GestaoDeEquipamentos.ConsoleApp/Entities/MaintenanceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Entities
+{
+    internal class MaintenanceRequest
+    {
+    }
+}
