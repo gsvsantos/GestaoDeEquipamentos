98b15bb1b9d6f82457be75773d7ab30b509bfce4
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 16:41:01 -0300

    feat: Adicionado método `ShowEquipmentList()` na `EquipmentManager.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
@@ -2,14 +2,15 @@
 
 public class EquipmentManager
 {
-    public Equipment[] EquipmentList;
+    public Equipment[] EquipmentList = new Equipment[100];
     public int equipmentListIndex = 0;
 
     public void RegisterEquipment()
     {
+        Console.Clear();
         Console.WriteLine("- Registro de Equipamento -");
         Console.Write("Nome do Equipamento: ");
-        string nome = Console.ReadLine()!;
+        string name = Console.ReadLine()!;
 
         Console.Write("Preço de Aquisição: ");
         double acquisitionPrice = Convert.ToDouble(Console.ReadLine());
@@ -20,7 +21,42 @@ public class EquipmentManager
         Console.Write("Data de Fabricação (dd/MM/yyyy): ");
         DateTime manufacturingDate = DateTime.Parse(Console.ReadLine()!);
 
-        Equipment newEquipment = new Equipment(nome, acquisitionPrice, manufacturingDate, manufacturer);
+        Equipment newEquipment = new Equipment(name, acquisitionPrice, manufacturingDate, manufacturer);
         EquipmentList[equipmentListIndex++] = newEquipment;
+    }
+    public void ShowEquipmentList(string typeList)
+    {
+        Console.WriteLine("- Equipamento Registrados -");
+        switch (typeList)
+        {
+            case "SEM-ID":
+                foreach (Equipment equipment in EquipmentList)
+                {
+                    if (EquipmentList[0] == null)
+                    {
+                        Console.WriteLine("Nenhum equipamento registrado!");
+                        break;
+                    }
+                    if (equipment != null)
+                        Console.WriteLine($"Nome: {equipment.Name}\nPreço: R$ {equipment.AcquisitionPrice}\nNúmero de Série: {equipment.SerialNumber}\nFabricante: {equipment.Manufacturer}\nData de Fabricação: {equipment.ManufacturingDate:dd/MM/yyyy}\n");
+                    else
+                        break;
+                }
+                break;
+            case "COM-ID":
+                foreach (Equipment equipment in EquipmentList)
+                {
+                    if (EquipmentList[0] == null)
+                    {
+                        Console.WriteLine("Nenhum equipamento registrado!");
+                        break;
+                    }
+                    if (equipment != null)
+                        Console.WriteLine($"ID: {equipment.Id}\nNome: {equipment.Name}\nPreço: R$ {equipment.AcquisitionPrice}\nNúmero de Série: {equipment.SerialNumber}\nFabricante: {equipment.Manufacturer}\nData de Fabricação: {equipment.ManufacturingDate:dd/MM/yyyy}\n");
+                    else
+                        break;
+                }
+                break;
+        }
     }
 }
