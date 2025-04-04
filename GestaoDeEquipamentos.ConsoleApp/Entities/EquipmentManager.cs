3fd737bec90f5240031cb919851353444c6ba79f
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 15:43:59 -0300

    feat: Adicionado método `RegisterEquipment()` na `EquipmentManager.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
@@ -3,4 +3,24 @@
 public class EquipmentManager
 {
     public Equipment[] EquipmentList;
+    public int equipmentListIndex = 0;
+
+    public void RegisterEquipment()
+    {
+        Console.WriteLine("- Registro de Equipamento -");
+        Console.Write("Nome do Equipamento: ");
+        string nome = Console.ReadLine()!;
+
+        Console.Write("Preço de Aquisição: ");
+        double acquisitionPrice = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nome do Fabricante: ");
+        string manufacturer = Console.ReadLine()!;
+
+        Console.Write("Data de Fabricação (dd/MM/yyyy): ");
+        DateTime manufacturingDate = DateTime.Parse(Console.ReadLine()!);
+
+        Equipment newEquipment = new Equipment(nome, acquisitionPrice, manufacturingDate, manufacturer);
+        EquipmentList[equipmentListIndex++] = newEquipment;
+    }
 }
