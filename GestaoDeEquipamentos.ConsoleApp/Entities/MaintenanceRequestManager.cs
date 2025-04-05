e600a3b455818bef6f9b1652049cf51e70d7262c
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 21:06:18 -0300

    feat: Adicionado `ShowMaintenanceRequestList()` na `MaintenanceRequestManager.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/MaintenanceRequestManager.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/MaintenanceRequestManager.cs
@@ -3,4 +3,30 @@
 public class MaintenanceRequestManager
 {
     public MaintenanceRequest[] MaintenanceRequestList = new MaintenanceRequest[100];
+
+    public void ShowMaintenanceRequestList(string typeList)
+    {
+        Console.WriteLine("- Chamados Registrados -");
+        foreach (MaintenanceRequest maintenanceRequest in MaintenanceRequestList)
+        {
+            if (MaintenanceRequestList == null)
+            {
+                Console.WriteLine("Nenhum chamado registrado!");
+                break;
+            }
+            switch (typeList)
+            {
+                case "SEM-ID":
+                    if (MaintenanceRequestList != null)
+                        Console.WriteLine($"Título: {maintenanceRequest.Title}\nEquipamento: {maintenanceRequest.Equipment.Name}\nData de Abertura: {maintenanceRequest.OpenDate:dd/MM/yyyy}\nDias Aberto: {maintenanceRequest.CalculateOpenDays()}\n");
+                    break;
+                case "COM-ID":
+                    if (MaintenanceRequestList != null)
+                        Console.WriteLine($"ID do Chamado: {maintenanceRequest.Id}\nTítulo: {maintenanceRequest.Title}\nDescrição: {maintenanceRequest.Description}\nEquipamento: {maintenanceRequest.Equipment.Name}\nData de Abertura: {maintenanceRequest.OpenDate:dd/MM/yyyy}\n");
+                    break;
+            }
+            if (MaintenanceRequestList == null)
+                break;
+        }
+    }
 }
