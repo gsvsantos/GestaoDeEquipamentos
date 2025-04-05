1a54b32bcb51a3ba526e989493946106a10b25e6
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-04 23:49:56 -0300

    refactor: Corrigida lógica de `EditEquipment()` na `EquipmentManager.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/EquipmentManager.cs
@@ -3,12 +3,14 @@
 public class EquipmentManager
 {
     public static Equipment[] EquipmentList = new Equipment[100];
-    public int EquipmentListIndex = 0;
+    public static int EquipmentListIndex = 0;
+    public static bool ListIsEmpty = false;
 
     public void RegisterEquipment()
     {
         Console.Clear();
         Console.WriteLine("- Registro de Equipamento -");
+
         Console.Write("Nome do Equipamento: ");
         string name = Console.ReadLine()!;
 
@@ -23,6 +25,10 @@ public class EquipmentManager
 
         Equipment newEquipment = new Equipment(name, acquisitionPrice, manufacturingDate, manufacturer);
         EquipmentList[EquipmentListIndex++] = newEquipment;
+
+        Console.WriteLine("\nEquipamento registrado com sucesso!");
+        Console.WriteLine("Pressione [Enter] para voltar ao menu!");
+        Console.ReadKey();
     }
     public void DeleteEquipment()
     {
@@ -30,28 +36,30 @@ public class EquipmentManager
         Console.WriteLine("- Exclusão de Equipamento -");
 
         ShowEquipmentList("COM-ID");
+        if (ListIsEmpty)
+            return;
 
         Console.Write("Digite o ID do equipamento que deseja excluir: ");
         int idChosen = Convert.ToInt32(Console.ReadLine());
 
         bool idFound = false;
+        Equipment equipmentChosen = null!;
         foreach (Equipment equipment in EquipmentList)
         {
             if (equipment == null)
                 continue;
             if (equipment.Id == idChosen)
             {
+                equipmentChosen = equipment;
                 idFound = true;
                 break;
             }
         }
-
         if (!idFound)
         {
             Console.WriteLine("Equipamento não encontrado, tente novamente!");
             return;
         }
-
 
         for (int i = 0; i < EquipmentList.Length; i++)
         {
@@ -60,9 +68,13 @@ public class EquipmentManager
             else if (EquipmentList[i].Id == idChosen)
             {
                 EquipmentList[i] = null!;
+                break;
             }
         }
+
         Console.WriteLine("\nO equipamento foi excluído com sucesso!");
+        Console.WriteLine("Pressione [Enter] para voltar ao menu!");
+        Console.ReadKey();
     }
     public void EditEquipment()
     {
@@ -70,22 +82,25 @@ public class EquipmentManager
         Console.WriteLine("- Edição de Equipamento -");
 
         ShowEquipmentList("COM-ID");
+        if (ListIsEmpty)
+            return;
 
         Console.Write("Digite o ID do equipamento que deseja editar: ");
         int idChosen = Convert.ToInt32(Console.ReadLine());
 
         bool idFound = false;
+        Equipment equipmentChosen = null!;
         foreach (Equipment equipment in EquipmentList)
         {
             if (equipment == null)
                 continue;
             if (equipment.Id == idChosen)
             {
+                equipmentChosen = equipment;
                 idFound = true;
                 break;
             }
         }
-
         if (!idFound)
         {
             Console.WriteLine("Equipamento não encontrado, tente novamente!");
@@ -93,42 +108,38 @@ public class EquipmentManager
         }
 
         Console.WriteLine("Digite abaixo os novos dados do equipamento: ");
+
         Console.Write("Nome do Equipamento: ");
-        string name = Console.ReadLine()!;
+        string newName = Console.ReadLine()!;
 
         Console.Write("Preço de Aquisição: ");
-        double acquisitionPrice = Convert.ToDouble(Console.ReadLine());
+        double newAcquisitionPrice = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Nome do Fabricante: ");
-        string manufacturer = Console.ReadLine()!;
+        string newManufacturer = Console.ReadLine()!;
 
         Console.Write("Data de Fabricação (dd/MM/yyyy): ");
-        DateTime manufacturingDate = DateTime.Parse(Console.ReadLine()!);
+        DateTime newManufacturingDate = DateTime.Parse(Console.ReadLine()!);
 
-        Equipment newEquipament = new Equipment(name, acquisitionPrice, manufacturingDate, manufacturer);
-        foreach (Equipment equipment in EquipmentList)
-        {
-            if (equipment == null)
-                continue;
-            else if (equipment.Id == idChosen)
-            {
-                equipment.Name = newEquipament.Name;
-                equipment.AcquisitionPrice = newEquipament.AcquisitionPrice;
-                equipment.ManufacturingDate = newEquipament.ManufacturingDate;
-                equipment.Manufacturer = newEquipament.Manufacturer;
-                break;
-            }
-        }
+        equipmentChosen.Name = newName;
+        equipmentChosen.AcquisitionPrice = newAcquisitionPrice;
+        equipmentChosen.Manufacturer = newManufacturer;
+        equipmentChosen.ManufacturingDate = newManufacturingDate;
+
         Console.WriteLine("\nO equipamento foi editado com sucesso!");
+        Console.WriteLine("Pressione [Enter] para voltar ao menu!");
+        Console.ReadKey();
     }
     public static void ShowEquipmentList(string typeList)
     {
         Console.WriteLine("- Equipamentos Registrados -");
+
         foreach (Equipment equipment in EquipmentList)
         {
             if (EquipmentList[0] == null)
             {
                 Console.WriteLine("Nenhum equipamento registrado!");
+                ListIsEmpty = true;
                 break;
             }
             switch (typeList)
@@ -145,6 +156,8 @@ public class EquipmentManager
             if (equipment == null)
                 break;
         }
+
+        Console.WriteLine("Pressione [Enter] para continuar!");
+        Console.ReadKey();
     }
 }
-
