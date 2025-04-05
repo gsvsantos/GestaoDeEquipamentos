c9ec3cca7e09ff58101b96640a3c2a74159ffbdd
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-05 05:39:54 -0300

    refactor: Adicionado opções de menus com o uso de suas respectivas classes.

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp
+﻿using GestaoDeEquipamentos.ConsoleApp.Entities;
+using GestaoDeEquipamentos.ConsoleApp.Entities.Utils;
+
+namespace GestaoDeEquipamentos.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Projeto - Gestão de Equipamentos!");
+            do
+            {
+                ShowMenu showMenu = new ShowMenu();
+                EquipmentManager equipmentManager = new EquipmentManager();
+                MaintenanceRequestManager maintenanceRequestManager = new MaintenanceRequestManager();
+
+                Console.Clear();
+                ViewWrite.ShowHeader("     Projeto - Gestão de Equipamentos");
+                showMenu.MainMenu();
+                Console.Write("Opção: ");
+                string option = Console.ReadLine()!;
+                switch (option)
+                {
+                    case "1":
+                        equipmentManager.EquipmentManagerOptions();
+                        break;
+                    case "2":
+                        maintenanceRequestManager.MaintenanceRequestManagerOptions();
+                        break;
+                    case "S":
+                        Console.WriteLine("Adeus (T_T)/");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            } while (true);
         }
     }
 }
