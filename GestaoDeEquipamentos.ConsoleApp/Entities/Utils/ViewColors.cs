0a58aae73e4d4ccd365e3cdccb4806891e77e130
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-05 02:22:32 -0300

    feat: Adicionado métodos de coloração na `ViewColors.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewColors.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewColors.cs
@@ -2,4 +2,16 @@
 
 public class ViewColors
 {
+    public static void WriteWithColor(string message, ConsoleColor color = ConsoleColor.White)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(message);
+        Console.ResetColor();
+    }
+    public static void WriteLineWithColor(string message, ConsoleColor color = ConsoleColor.White)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
 }
