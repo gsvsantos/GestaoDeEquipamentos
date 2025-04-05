302cee7cfddaa77b9494bd5816bcad5e45d27805
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-05 03:54:11 -0300

    feat: Adicionado método `ShowHeader()` na `ViewWrite.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewWrite.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewWrite.cs
@@ -2,4 +2,14 @@
 
 public class ViewWrite
 {
+    public static void ShowHeader(string title, int linesQuantity = 40)
+    {
+        Console.WriteLine("/" + new string('=', linesQuantity) + "\\");
+        Console.WriteLine(title.ToUpper());
+        Console.WriteLine("\\" + new string('=', linesQuantity) + "/");
+    }
+    public static void ShowTable()
+    {
+
+    }
 }
