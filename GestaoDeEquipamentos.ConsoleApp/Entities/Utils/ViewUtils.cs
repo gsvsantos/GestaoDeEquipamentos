206584e932267e0917f5725afd29a8aaa39312aa
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-05 21:52:25 -0300

    refactor: Adicionado `GetEquipmentChosen()` na `ViewUtils.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewUtils.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Utils/ViewUtils.cs
@@ -13,4 +13,33 @@ public class ViewUtils
         ViewColors.WriteWithColor("\nPressione [Enter] para continuar.", ConsoleColor.Yellow);
         Console.ReadKey();
     }
+    public static Equipment GetEquipmentChosen(string prompt, string idNotFoundError, EquipmentManager equipmentManager)
+    {
+        do
+        {
+            ViewColors.WriteWithColor(prompt);
+            int idChosen = Convert.ToInt32(Console.ReadLine());
+
+            bool idFound = false;
+            Equipment equipmentChosen = null!;
+            foreach (Equipment equipment in EquipmentManager.EquipmentList)
+            {
+                if (equipment == null)
+                    continue;
+                if (equipment.Id == idChosen)
+                {
+                    equipmentChosen = equipment;
+                    idFound = true;
+                    break;
+                }
+            }
+            if (!idFound)
+            {
+                ViewColors.WriteLineWithColor($"{idNotFoundError}");
+                continue;
+            }
+
+            return equipmentChosen;
+        } while (true);
+    }
 }
