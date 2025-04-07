731d2869735c17885f3defaa9fa72ef12a03fa8d
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-07 20:13:37 -0300

    feat: Adicionado método `GenerateId()` na `Manufacturer.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Manufacturer.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Manufacturer.cs
@@ -6,4 +6,8 @@ public class Manufacturer : Entity
     public string Email;
     public string Phone;
     private static int id = 0;
+    public int GenerateId()
+    {
+        return Id = ++id;
+    }
 }
