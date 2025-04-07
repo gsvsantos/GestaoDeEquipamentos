29204d5fe8ecabcf88c9c9a21fada6c79e0b67b3
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-07 19:07:50 -0300

    feat: Adicionado método `GenerateId()` para melhor organização dos equipamentos.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Equipment.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Equipment.cs
@@ -4,25 +4,25 @@ public class Equipment : Entity
 {
     public string Name;
     public double AcquisitionPrice;
-    public string SerialNumber;
     public DateTime ManufacturingDate;
     public string Manufacturer;
-    public static int id = 0;
+    private static int id = 0;
 
     public Equipment(string name, double acquisitionPrice, DateTime manufacturingDate, string manufacturer)
     {
-        Id = ++id;
         Name = name;
         AcquisitionPrice = acquisitionPrice;
-        SerialNumber = GetSerialNumber();
         ManufacturingDate = manufacturingDate;
         Manufacturer = manufacturer;
     }
-
+    public int GenerateId()
+    {
+        return Id = ++id;
+    }
     public string GetSerialNumber()
     {
         string firstThreeChars = Name.Substring(0, 3).ToUpper();
 
-        return $"{firstThreeChars}-{id}";
+        return $"{firstThreeChars}-{Id}";
     }
 }
