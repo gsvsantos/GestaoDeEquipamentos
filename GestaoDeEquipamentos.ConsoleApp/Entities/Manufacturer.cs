da89d981f9d2792f2eb1d777dd8fcecae4393867
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-07 20:14:04 -0300

    feat: Adicionado construtor da `Manufacturer.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Entities/Manufacturer.cs
+++ GestaoDeEquipamentos.ConsoleApp/Entities/Manufacturer.cs
@@ -6,6 +6,13 @@ public class Manufacturer : Entity
     public string Email;
     public string Phone;
     private static int id = 0;
+
+    public Manufacturer(string name, string email, string phone)
+    {
+        Name = name;
+        Email = email;
+        Phone = phone;
+    }
     public int GenerateId()
     {
         return Id = ++id;
