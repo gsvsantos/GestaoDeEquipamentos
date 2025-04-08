9a6d90adbc2a51abc5330edfbb9fcf4662bc6576
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-08 00:02:55 -0300

    feat: Adicionado método `RegisterManufacturer()` na `ManufacturerRepository.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Repositories/ManufacturerRepository.cs
+++ GestaoDeEquipamentos.ConsoleApp/Repositories/ManufacturerRepository.cs
@@ -7,4 +7,10 @@ public class ManufacturerRepository
     public Manufacturer[] ManufacturerList = new Manufacturer[100];
     public int ManufacturerListIndex = 0;
     public bool ListIsEmpty = false;
+
+    public void RegisterManufacturer(Manufacturer newManufacturer)
+    {
+        newManufacturer.GenerateId();
+        ManufacturerList[ManufacturerListIndex] = newManufacturer;
+    }
 }
