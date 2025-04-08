797d8abf260d2561bbb03e1e3cbbb62469d90517
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-07 23:56:56 -0300

    feat: Adicionado atributos na `ManufacturerRepository.cs`.

--- GestaoDeEquipamentos.ConsoleApp/Repositories/ManufacturerRepository.cs
+++ GestaoDeEquipamentos.ConsoleApp/Repositories/ManufacturerRepository.cs
@@ -1,5 +1,10 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp.Repositories;
+﻿using GestaoDeEquipamentos.ConsoleApp.Entities;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Repositories;
 
 public class ManufacturerRepository
 {
+    public Manufacturer[] ManufacturerList = new Manufacturer[100];
+    public int ManufacturerListIndex = 0;
+    public bool ListIsEmpty = false;
 }
